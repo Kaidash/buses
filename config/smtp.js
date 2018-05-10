191bdf64dfcc81abf7dba90e97b9a9e30d453010
Kaidash <kaidash.nikita93@gmail.com>
2018-05-10 11:41:07 +0300

    change port

--- config/smtp.js
+++ config/smtp.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'smtp_host': 'smtp.gmail.com',
-	'smtp_port': '465',
+	'smtp_port': '587',
 	'smtp_email': 'Bus.reserv@gmail.com',
 	'smtp_password': 'admin_admin',
 	'email_to': 'Bus.reserv@gmail.com'
